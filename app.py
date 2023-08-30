93370509543768977083599623433a069b7d9d97
Emmanuel Akanbi <emmanuelakanbi.8@gmail.com>
2023-08-29 22:21:51 -0500

    update

--- app.py
+++ app.py
@@ -13,9 +13,26 @@ app.secret_key = b'B\xdak\x81\x1dmS\xa8I\xb6'
 def index():
     return render_template('index.html')
 
-@app.route('/calories')
+@app.route('/calories', methods=['GET', 'POST'])
 def calories():
     form = CaloriesForm()
+
+    if request.method == 'POST':
+        temperature = Temperature(country=form.country.data,
+                                  city=form.city.data).get()
+        
+        calorie = Calorie(weight=form.weight.data,
+                           height=form.height.data, 
+                           age=form.age.data, 
+                           temperature=temperature)                
+        calories = calorie.calculate()
+        
+        
+        
+        return render_template('calories_form_page.html', title = 'Calories', 
+                           caloriesform=form,
+                            calories = calories,
+                            result=True)
     return render_template('calories_form_page.html', title = 'Calories', 
                            caloriesform=form)
 
